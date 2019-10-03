2a53c680a0b604953a21cc107821b689b54ce23b
aman-maharshi <amanm4harshi@gmail.com>
2019-10-03 21:03:35 +0530

    level up counter added

--- basic-workflow/src/Tweet.js
+++ basic-workflow/src/Tweet.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css'
 
 // function Tweet(props) {
@@ -11,12 +11,22 @@ import './App.css'
 //     );
 // }
 
+
 function Tweet({name, role}) {
+
+    const [count, setCount] = useState(0);
+    // count - variable, setCount - function that changes the variable, useState(0) - initail value of state
+
+    const increment = () => {
+        setCount(count + 1);
+    }
+
     return(
         <div className="Tweet">
             <h3>Player : {name}</h3>
             <p>This is a random Tweet</p>
             <p className="Role">Role : {role}</p>
+            <p><button onClick={increment}><i class="fas fa-thumbs-up">Level Up</i></button> : <span>{count}</span></p>
         </div>
     );
 }
