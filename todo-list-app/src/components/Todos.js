af11691719ba621c17ee9e850b8ca8bd7b65290a
aman-maharshi <amanm4harshi@gmail.com>
2020-07-28 00:01:38 +0530

    first component

---
+++ todo-list-app/src/components/Todos.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../App.css';
+
+function Todos() {
+  return (
+    <>
+      <header>
+        <p className="title">Todo App</p>
+      </header>
+    </>
+  );
+}
+
+export default Todos;
