80da8944e2780b52ec3dc8dccd30007dd5c8250d
aman-maharshi <amanm4harshi@gmail.com>
2020-08-05 12:51:20 +0530

    intial components added

--- 10-days-of-react/src/App.js
+++ 10-days-of-react/src/App.js
@@ -3,10 +3,33 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Welcome</h1>
+    <div className="app">
+      <Header />
+      <TimeArea />
+      <Footer />
     </div>
   );
 }
+
+function Header() {
+  return (
+    <h1 className="title">App Header</h1>
+  );
+}
+
+function TimeArea() {
+  return (
+    <p>Time: <span className="time">{new Date().toLocaleString()}</span></p>
+  );
+}
+
+function Footer() {
+  return (
+    <footer>
+      <p>Coded by <a href="">Aman Maharshi</a></p>
+    </footer>
+  );
+}
+
 
 export default App;
