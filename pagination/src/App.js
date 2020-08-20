9b7e5288774fe911cb3219f7452ba9351e375d96
aman-maharshi <amanm4harshi@gmail.com>
2020-08-20 18:01:22 +0530

    init

---
+++ pagination/src/App.js
@@ -0,0 +1,12 @@
+import React from "react"
+import "./App.css"
+
+function App() {
+    return (
+        <div className="container">
+            <h1>Simple Pagination in React</h1>
+        </div>
+    )
+}
+
+export default App
