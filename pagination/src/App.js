b77311f6f988d9f4b3bcd740caaa29fbfe48b1bf
aman-maharshi <amanm4harshi@gmail.com>
2020-08-20 18:23:59 +0530

    making axios request

--- pagination/src/App.js
+++ pagination/src/App.js
@@ -1,7 +1,25 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
+import axios from "axios"
 import "./App.css"
 
 function App() {
+    const [post, setPost] = useState([])
+    const [loading, setLoading] = useState(false)
+    const [currentPage, setCurrentPage] = useState(1)
+    const [postsPerPage, setPostsPerPage] = useState(5)
+
+    useEffect(() => {
+        async function fetchPosts() {
+            setLoading(true)
+            const response = await axios.get("https://jsonplaceholder.typicode.com/posts")
+            setPost(response.data)
+            setLoading(false)
+        }
+        fetchPosts()
+    }, [])
+
+    console.log(post)
+
     return (
         <div className="container">
             <h1>Simple Pagination in React</h1>
