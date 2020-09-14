126eb113b831bfe39ea85774b44818b858947bfa
aman-maharshi <amanm4harshi@gmail.com>
2020-09-14 19:13:21 +0530

    Updates

---
+++ precache-manifest.9d0fccb46c2088834344d850bf71fdaa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e067c6f8bbf93374f179a5a13f8f0ed7",
+    "url": "/react-js-hands-on/todo-simple/index.html"
+  },
+  {
+    "revision": "5413b59b8c8f41a3b6f4",
+    "url": "/react-js-hands-on/todo-simple/static/css/main.05c04535.chunk.css"
+  },
+  {
+    "revision": "5632d9876c0e3efd1bc4",
+    "url": "/react-js-hands-on/todo-simple/static/js/2.9352cc44.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-js-hands-on/todo-simple/static/js/2.9352cc44.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5413b59b8c8f41a3b6f4",
+    "url": "/react-js-hands-on/todo-simple/static/js/main.3010c5d3.chunk.js"
+  },
+  {
+    "revision": "87a19135a8839eeee81f",
+    "url": "/react-js-hands-on/todo-simple/static/js/runtime-main.0900e161.js"
+  }
+]);
