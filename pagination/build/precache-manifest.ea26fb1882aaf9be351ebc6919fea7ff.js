0ef168cbf91bae5cc2c89b947250304ee4d2097d
aman-maharshi <amanm4harshi@gmail.com>
2020-09-15 09:33:23 +0530

    pagination build added

---
+++ pagination/build/precache-manifest.ea26fb1882aaf9be351ebc6919fea7ff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1459e82705e7f26ce03a6df98c990dc4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "09c2e6a3dd8cd6c0ed80",
+    "url": "/static/css/main.c0ce9ea7.chunk.css"
+  },
+  {
+    "revision": "86656dfa0f1de5446e10",
+    "url": "/static/js/2.349d2ba9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.349d2ba9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "09c2e6a3dd8cd6c0ed80",
+    "url": "/static/js/main.833d44ba.chunk.js"
+  },
+  {
+    "revision": "96992be1975bdf42ceea",
+    "url": "/static/js/runtime-main.aaf85a40.js"
+  }
+]);
