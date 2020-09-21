ae4af51c27263c6cc32d901224dd186d670f071e
aman-maharshi <amanm4harshi@gmail.com>
2020-09-21 16:50:06 +0530

    remove item and save and load from localstorage added

--- practice-2/src/App.js
+++ practice-2/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react"
+import React, { useState, useEffect } from "react"
 import "./index.css"
 
 // Components
@@ -6,12 +6,42 @@ import DateTime from "./DateTime"
 import LikeArea from "./LikeArea"
 import AddTodo from "./AddTodo"
 
-function TodoItem({ todo }) {
-    return <li className="TodoItem">{todo}</li>
+function TodoItem({ todo, todos, setTodos }) {
+    function handleRemoveItem(e) {
+        e.preventDefault()
+        const textWithX = e.target.parentElement.textContent
+        const textItem = textWithX.substring(0, textWithX.length - 2)
+        const index = todos.indexOf(textItem)
+        /* const arr = [...todos] */
+        const arr = JSON.parse(localStorage.getItem("todoItems"))
+        arr.splice(index, 1)
+        setTodos(arr)
+    }
+
+    return (
+        <li className="TodoItem">
+            {todo}{" "}
+            <button onClick={handleRemoveItem} className="remove-item">
+                X
+            </button>
+        </li>
+    )
 }
 
 function App() {
-    const [todos, setTodos] = useState(["Practice  Web Programming", "Practice Guitar", "Go to Gym"])
+    const [todos, setTodos] = useState(["Practice Guitar", "Go to Gym"])
+
+    // run first time on refresh / page load
+    useEffect(() => {
+        if (localStorage.getItem("todoItems")) {
+            setTodos(JSON.parse(localStorage.getItem("todoItems")))
+        }
+    }, [])
+
+    // run everytime todos changes
+    useEffect(() => {
+        localStorage.setItem("todoItems", JSON.stringify(todos))
+    }, [todos])
 
     return (
         <div className="App">
@@ -20,7 +50,7 @@ function App() {
             <AddTodo setTodos={setTodos} />
             <ul className="TodoList">
                 {todos.map(item => {
-                    return <TodoItem todo={item} key={Math.floor(Math.random() * 1000)} />
+                    return <TodoItem setTodos={setTodos} todo={item} todos={todos} key={Math.floor(Math.random() * 1000)} />
                 })}
             </ul>
             <LikeArea />
