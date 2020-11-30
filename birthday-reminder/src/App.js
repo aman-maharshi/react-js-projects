70c9579a4043e3fb8cf542068817ec5f63674278
aman-maharshi <amanm4harshi@gmail.com>
2020-11-30 22:43:15 +0530

    funtionality and styling complete

--- birthday-reminder/src/App.js
+++ birthday-reminder/src/App.js
@@ -1,9 +1,46 @@
-function App() {
-    return (
-        <div>
-            <p>Hi</p>
-        </div>
-    )
+import React from "react"
+import data from "./data.js"
+
+class App extends React.Component {
+    state = {
+        listItems: data
+    }
+
+    clearAll = () => {
+        this.setState({
+            listItems: []
+        })
+    }
+
+    render() {
+        const listItems = this.state.listItems
+
+        return (
+            <>
+                <main className="container">
+                    <p className="title">{listItems.length} Birthdays Today</p>
+                    <div className="list">
+                        {listItems.map(item => {
+                            return (
+                                <div className="person" key={item.id}>
+                                    <div className="person__photo">
+                                        <img src={item.image} alt={item.name} />
+                                    </div>
+                                    <div>
+                                        <p className="person__name">{item.name}</p>
+                                        <p className="person__age">{item.age} years</p>
+                                    </div>
+                                </div>
+                            )
+                        })}
+                    </div>
+                    <button onClick={this.clearAll} disabled={listItems.length ? false : true}>
+                        Clear All
+                    </button>
+                </main>
+            </>
+        )
+    }
 }
 
 export default App
