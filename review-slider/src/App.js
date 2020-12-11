32a2495288570f79dfd975b10efe8e28c3b1592e
aman-maharshi <amanm4harshi@gmail.com>
2020-12-11 20:58:36 +0530

    functionality complete

--- review-slider/src/App.js
+++ review-slider/src/App.js
@@ -2,10 +2,28 @@ import React, { useState } from "react"
 import data from "./data"
 
 function App() {
-    const [reviews, setReview] = useState(data)
+    const [reviews] = useState(data)
     const [currentIndex, setCurrentIndex] = useState(0)
 
     const { id, image, job, name, text } = reviews[currentIndex]
+
+    const nextReview = () => {
+        setCurrentIndex(currentValue => {
+            if (currentIndex === reviews.length - 1) {
+                return 0
+            }
+            return currentValue + 1
+        })
+    }
+
+    const prevReview = () => {
+        setCurrentIndex(currentValue => {
+            if (currentIndex === 0) {
+                return reviews.length - 1
+            }
+            return currentValue - 1
+        })
+    }
 
     return (
         <div>
@@ -22,10 +40,18 @@ function App() {
                     </div>
                 </div>
                 <div className="button-box">
-                    <button>Prev</button>
-                    <button>Next</button>
+                    <button onClick={prevReview}>Prev</button>
+                    <button onClick={nextReview}>Next</button>
                 </div>
             </div>
+            <footer>
+                <p>
+                    Coded by{" "}
+                    <a href="https://amanmaharshi.com" rel="noreferrer" target="_blank">
+                        Aman Maharshi
+                    </a>{" "}
+                </p>
+            </footer>
         </div>
     )
 }
--- review-slider/src/data.js
+++ review-slider/src/data.js
@@ -2,7 +2,7 @@ const reviews = [
     {
         id: 1,
         name: "susan smith",
-        job: "web developer",
+        job: "front end developer",
         image: "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg",
         text: "I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry"
     },
@@ -16,14 +16,14 @@ const reviews = [
     {
         id: 3,
         name: "peter jones",
-        job: "intern",
+        job: "business analyst",
         image: "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg",
         text: "Sriracha literally flexitarian irony, vape marfa unicorn. Glossier tattooed 8-bit, fixie waistcoat offal activated charcoal slow-carb marfa hell of pabst raclette post-ironic jianbing swag."
     },
     {
         id: 4,
         name: "bill anderson",
-        job: "the boss",
+        job: "back end developer",
         image: "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg",
         text: "Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. "
     }
