48337e924264667e02ace8e3309fd99d38eb50b5
aman-maharshi <amanm4harshi@gmail.com>
2020-12-14 19:46:55 +0530

    single item toggle functionality added

--- accordion/src/App.js
+++ accordion/src/App.js
@@ -1,10 +1,24 @@
-import React from "react"
+import React, { useState } from "react"
 
 function App() {
+    const [answerVisible, setAnswerVisible] = useState(false)
+
+    const toggleAccordion = () => {
+        setAnswerVisible(!answerVisible)
+    }
+
     return (
-        <div>
-            <p>Hi</p>
-        </div>
+        <section className="main-container">
+            <div className="accordion">
+                <div className="question">
+                    Lorem ipsum dolor sit amet?{" "}
+                    <span className="toggle" onClick={toggleAccordion}>
+                        {answerVisible ? "-" : "+"}
+                    </span>
+                </div>
+                <div className={"answer" + (answerVisible ? " show-answer" : "")}>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Neque, placeat ex. Nesciunt nulla doloribus architecto, minima reprehenderit ad soluta sapiente quaerat ea aperiam corrupti accusamus dignissimos velit laborum quasi error?</div>
+            </div>
+        </section>
     )
 }
 
