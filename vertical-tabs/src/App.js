88cf6d0e3a93fc35813185f3788695508c1171c8
aman-maharshi <amanm4harshi@gmail.com>
2021-03-11 15:01:27 +0530

    functionality complete

--- vertical-tabs/src/App.js
+++ vertical-tabs/src/App.js
@@ -1,12 +1,52 @@
 import React, { useState, useEffect } from "react"
 
-const apiUrl = "https://course-api.com/react-tabs-project"
-
 function App() {
+    const [loading, setLoading] = useState(true)
+    const [jobs, setJobs] = useState([])
+    const [jobIndex, setJobIndex] = useState(0)
+
+    const apiUrl = "https://course-api.com/react-tabs-project"
+
+    const fetchJobs = async () => {
+        const response = await fetch(apiUrl)
+        const data = await response.json()
+        setJobs(data)
+        setLoading(false)
+    }
+
+    useEffect(() => {
+        fetchJobs()
+    }, [])
+
     return (
-        <div>
-            <p>Hi</p>
-        </div>
+        <>
+            <h1>Work Experience</h1>
+            {!loading ? (
+                <main>
+                    <div className="left">
+                        {jobs.map((item, index) => {
+                            return (
+                                <button onClick={() => setJobIndex(index)} key={item.id} className={`${index === jobIndex && "btn-active"}`}>
+                                    {item.company}
+                                </button>
+                            )
+                        })}
+                    </div>
+                    <div className="right">
+                        <h2>{jobs[jobIndex].title}</h2>
+                        <h3>{jobs[jobIndex].company}</h3>
+                        <p>{jobs[jobIndex].dates}</p>
+                        <ul>
+                            {jobs[jobIndex].duties.map((item, index) => {
+                                return <li key={index}>{item}</li>
+                            })}
+                        </ul>
+                    </div>
+                </main>
+            ) : (
+                <p className="loading">Loading...</p>
+            )}
+        </>
     )
 }
 
