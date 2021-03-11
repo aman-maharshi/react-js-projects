2b1ef42d5101e13e2a4b8f9865983f8a85ee2585
aman-maharshi <amanm4harshi@gmail.com>
2021-03-11 12:41:52 +0530

    vertial tabs project init

---
+++ vertical-tabs/src/App.js
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from "react"
+
+const apiUrl = "https://course-api.com/react-tabs-project"
+
+function App() {
+    return (
+        <div>
+            <p>Hi</p>
+        </div>
+    )
+}
+
+export default App
