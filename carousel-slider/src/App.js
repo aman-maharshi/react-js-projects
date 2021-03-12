33ddb58df5387467b9941e5ef5f08aef29e89712
aman-maharshi <amanm4harshi@gmail.com>
2021-03-12 15:40:14 +0530

    data and layout setup

--- carousel-slider/src/App.js
+++ carousel-slider/src/App.js
@@ -1,9 +1,22 @@
-import React from "react"
+import React, { useState } from "react"
+import imageData from "./data"
 
 function App() {
+    const [data, setData] = useState(imageData)
+
     return (
-        <div>
-            <p>Hi</p>
+        <div className="slider">
+            <div className="slider__wrapper">
+                {data.map(item => {
+                    const { title, image } = item
+                    return (
+                        <div className="slider__content">
+                            <img src={image} alt={title} />
+                            <span className="title">{title}</span>
+                        </div>
+                    )
+                })}
+            </div>
         </div>
     )
 }
---
+++ carousel-slider/src/data.js
@@ -0,0 +1,15 @@
+const data = [
+    {
+        title: "One",
+        image: "./images/one.png"
+    },
+    {
+        title: "Two",
+        image: "./images/two.png"
+    },
+    {
+        title: "Three",
+        image: "./images/three.png"
+    }
+]
+export default data
