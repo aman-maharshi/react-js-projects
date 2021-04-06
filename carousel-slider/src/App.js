c1a18702792524f343f0380e3b8646f3ad0d7a43
aman-maharshi <amanm4harshi@gmail.com>
2021-04-06 22:07:02 +0530

    styling setup for the carousel

--- carousel-slider/src/App.js
+++ carousel-slider/src/App.js
@@ -3,14 +3,15 @@ import imageData from "./data"
 
 function App() {
     const [data, setData] = useState(imageData)
+    const [index, setIndex] = useState(0)
 
     return (
         <div className="slider">
             <div className="slider__wrapper">
-                {data.map(item => {
+                {data.map((item, index) => {
                     const { title, image } = item
                     return (
-                        <article>
+                        <article key={index}>
                             <img src={image} alt={title} />
                             <p className="title">{title}</p>
                         </article>
