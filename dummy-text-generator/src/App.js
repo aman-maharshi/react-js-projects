78abe4cff1bfd8ec3405addfbbb47498b52861fe
aman-maharshi <amanm4harshi@gmail.com>
2021-04-10 23:13:14 +0530

    simple implementation of words generator based on selected length

--- dummy-text-generator/src/App.js
+++ dummy-text-generator/src/App.js
@@ -1,9 +1,18 @@
-import React from "react"
+import React, { useState } from "react"
 
 function App() {
+    const [words, setWords] = useState(["Lorem.", "Lorem, ipsum.", "Lorem, ipsum dolor.", "Lorem ipsum dolor sit.", "Lorem ipsum dolor sit amet.", "Lorem ipsum dolor sit amet consectetur.", "Lorem ipsum dolor sit amet consectetur adipisicing."])
+    const [length, setLength] = useState(0)
     return (
         <div>
-            <p>Hi</p>
+            <form>
+                <p>Select a Length</p>
+                <div className="inputbox">
+                    <input type="range" min="0" max="7" value={length} onChange={e => setLength(e.target.value)} />
+                    <span>{length}</span>
+                </div>
+            </form>
+            <div className="result">{length ? words[length - 1] : null}</div>
         </div>
     )
 }
