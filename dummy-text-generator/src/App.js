7119c6baa0bf24450d8749fa3702ef85bb40d613
aman-maharshi <amanm4harshi@gmail.com>
2021-04-12 19:32:20 +0530

    functionality and styling complete

--- dummy-text-generator/src/App.js
+++ dummy-text-generator/src/App.js
@@ -1,18 +1,29 @@
 import React, { useState } from "react"
+import data from "./data"
 
 function App() {
-    const [words, setWords] = useState(["Lorem.", "Lorem, ipsum.", "Lorem, ipsum dolor.", "Lorem ipsum dolor sit.", "Lorem ipsum dolor sit amet.", "Lorem ipsum dolor sit amet consectetur.", "Lorem ipsum dolor sit amet consectetur adipisicing."])
-    const [length, setLength] = useState(0)
+    const [text, setText] = useState([])
+    const [paragraphCount, setParagraphCount] = useState(0)
+
+    const generateParagraphs = e => {
+        e.preventDefault()
+        setText(data.slice(0, paragraphCount))
+    }
     return (
         <div>
-            <form>
-                <p>Select a Length</p>
+            <form onSubmit={generateParagraphs}>
+                <p>Select the number of paragraphs you want</p>
                 <div className="inputbox">
-                    <input type="range" min="0" max="7" value={length} onChange={e => setLength(e.target.value)} />
-                    <span>{length}</span>
+                    <input type="range" min="0" max="9" value={paragraphCount} onChange={e => setParagraphCount(e.target.value)} />
+                    <span>{paragraphCount}</span>
+                    <button type="submit">Generate</button>
                 </div>
             </form>
-            <div className="result">{length ? words[length - 1] : null}</div>
+            <div className="result">
+                {text.map((paragraph, index) => {
+                    return <p key={index}>{paragraph}</p>
+                })}
+            </div>
         </div>
     )
 }
