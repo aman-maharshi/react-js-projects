a154e52befc0a9103eb74e42a3fd55bb94a64c2f
aman-maharshi <amanm4harshi@gmail.com>
2021-04-17 19:55:49 +0530

    init and clean up

---
+++ color-generator/src/App.js
@@ -0,0 +1,17 @@
+import React from "react"
+
+function App() {
+    return (
+        <div className="wrapper">
+            <h3>Lorem ipsum dolor sit amet.</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum, mollitia dicta, fugit totam commodi soluta laudantium impedit quaerat vel facere quia temporibus. Iste, sint perferendis!</p>
+
+            <form className="form">
+                <input type="text" />
+                <button type="submit">Generate</button>
+            </form>
+        </div>
+    )
+}
+
+export default App
