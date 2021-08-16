c3f792aef1ad94f7f374c38f681d26ac007580c2
aman-maharshi <amanm4harshi@gmail.com>
2021-08-16 16:49:19 +0530

    - pat creation test

--- color-generator/src/App.js
+++ color-generator/src/App.js
@@ -1,15 +1,37 @@
-import React from "react"
+import React, { useState } from "react"
 
 function App() {
+    const [userInput, setUserInput] = useState("")
+    const [error, setError] = useState(false)
+
+    const handleSubmit = e => {
+        e.preventDefault()
+        try {
+            console.log(userInput)
+            setError(false)
+        } catch (e) {
+            console.log(e)
+            setError(true)
+        }
+    }
+
     return (
         <div className="wrapper">
-            <h3>Lorem ipsum dolor sit amet.</h3>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum, mollitia dicta, fugit totam commodi soluta laudantium impedit quaerat vel facere quia temporibus. Iste, sint perferendis!</p>
+            <h3>Color Shade Generator</h3>
+            <p>Enter a color code to get its different shades. Ex : #e74c3c, #27ae60 </p>
 
-            <form className="form">
-                <input type="text" />
+            <form className="form" onSubmit={handleSubmit}>
+                <input type="text" value={userInput} onChange={e => setUserInput(e.target.value)} placeholder="Hex Color Code" />
                 <button type="submit">Generate</button>
             </form>
+            {error ? <span className="error">Invalid Color Code</span> : null}
+            <div>{/* {colorShades.map(color => {
+                    return (
+                        <ul>
+                            <li>{color.hex}</li>
+                        </ul>
+                    )
+                })} */}</div>
         </div>
     )
 }
