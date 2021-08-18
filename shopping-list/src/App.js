78ed712a24ee5751049a3709ee79759abcdfcf94
aman-maharshi <amanm4harshi@gmail.com>
2021-08-18 21:19:41 +0530

    project init

---
+++ shopping-list/src/App.js
@@ -0,0 +1,14 @@
+function App() {
+    return (
+        <>
+            <div className="page-content"></div>
+            <footer>
+                <p>
+                    Designed and Coded by <a href="https://www.linkedin.com/in/amanmaharshi/">Aman Maharshi</a>
+                </p>
+            </footer>
+        </>
+    )
+}
+
+export default App
