c2604c40811bb8be1da702892b2dceb2d21b453d
aman-maharshi <amanm4harshi@gmail.com>
2021-08-20 15:20:54 +0530

    overall app styling complete

--- shopping-list/src/App.js
+++ shopping-list/src/App.js
@@ -1,7 +1,26 @@
+import { useState } from "react"
+
 function App() {
+    const [showMessage, setShowMessage] = useState(false)
+    const [message, setMessage] = useState("message")
+
     return (
         <>
-            <div className="page-content">Hello there!</div>
+            <div className="page-content">
+                <div className="app">
+                    <h3>Shopping List</h3>
+                    <form className="form">
+                        <input type="text" />
+                        <button type="submit">Add</button>
+                        {showMessage && <div className="message">{message}</div>}
+                    </form>
+
+                    <div>
+                        <p className="todo-item">Bread</p>
+                        <p className="todo-item">Milk</p>
+                    </div>
+                </div>
+            </div>
             <footer>
                 <p>
                     Designed and Coded by <a href="https://www.linkedin.com/in/amanmaharshi/">Aman Maharshi</a>
