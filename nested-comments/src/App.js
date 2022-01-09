57e562b92b689c6429c2d0d37642f94d92e4d2ee
aman-maharshi <amanm4harshi@gmail.com>
2022-01-09 17:59:08 +0530

    clean up after react app init

--- nested-comments/src/App.js
+++ nested-comments/src/App.js
@@ -1,25 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <div>
+      <h2>Lorem, ipsum dolor.</h2>
+      <img src="/images/userLogo.jpeg" alt="userLogo"/>
+    </div>    
+  )
 }
 
 export default App;
