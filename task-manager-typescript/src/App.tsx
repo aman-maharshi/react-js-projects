7336241e5a9db480ea74773c37f0612cc28a141f
aman-maharshi <amanm4harshi@gmail.com>
2022-10-05 11:22:17 +0530

    typescript and tailwind init

---
+++ task-manager-typescript/src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+import "./index.css"
+
+function App() {
+    return (
+        <div className="p-4">
+            <h1 className="text-3xl font-bold">Hello world!</h1>
+        </div>
+    )
+}
+
+export default App
