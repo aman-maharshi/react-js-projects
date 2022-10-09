9282ee31eeb25d2ea4aa4240d4cefcafd40f0ea1
aman-maharshi <amanm4harshi@gmail.com>
2022-10-09 18:30:53 +0530

    edit item functionality

--- task-manager-typescript/src/SingleTodo.tsx
+++ task-manager-typescript/src/SingleTodo.tsx
@@ -1,4 +1,4 @@
-import React from "react"
+import { useState, useRef, useEffect } from "react"
 import { Todo } from "./model"
 import { MdEdit, MdDelete, MdCheck } from "react-icons/md"
 
@@ -9,6 +9,18 @@ interface Props {
 }
 
 const SingleTodo = ({ item, todos, setTodos }: Props) => {
+    const [enableEdit, setEnableEdit] = useState<boolean>(false)
+    const [todoText, setTodoText] = useState<string>(item.text)
+    const editInputRef = useRef<HTMLInputElement>(null)
+
+    useEffect(() => {
+        if (enableEdit) {
+            if(editInputRef.current) {
+                editInputRef.current.focus()
+            }
+        }
+    }, [enableEdit])
+    
 
     const handleDelete = () => {
         setTodos(todos.filter(listItem => listItem.id !== item.id ))
@@ -17,21 +29,52 @@ const SingleTodo = ({ item, todos, setTodos }: Props) => {
     const toggleCompleted = () => {
       setTodos(todos.map(listItem => {
           if (listItem.id === item.id) {
-              return {
+              return ({
                   ...listItem,
                   completed: !listItem.completed
-              }
+              })
           } else {
               return listItem
           }
       }))
     }
 
+    const handleEdit = () => {
+        if (!item.completed) {
+            setEnableEdit(!enableEdit)
+        }
+    }
+
+    const handleEditFormSubmit = (e:  React.FormEvent) => {
+        e.preventDefault()
+        if (todoText) {
+            setTodos(todos.map(listItem => {
+                if(listItem.id === item.id) {
+                    return ({
+                        ...listItem,
+                        text: todoText
+                    })
+                } else {
+                    return listItem
+                }
+            }))
+        }
+        setEnableEdit(false)
+    }
+
     return (
         <div className="mb-2 p-2 bg-yellow-200 border-b-2 border-yellow-500 flex justify-between">
-            <div className={item.completed ?"flex-1 truncate line-through" : "flex-1 truncate" }>{item.text}</div>
+            <div className={item.completed ?"flex-1 truncate line-through" : "flex-1 truncate" }>
+                {enableEdit ? (
+                    <form onSubmit={handleEditFormSubmit}>
+                        <input value={todoText} onChange={e => setTodoText(e.target.value)} ref={editInputRef} type="text" className="focus:outline-blue-500" />
+                    </form>
+                ) : (
+                    item.text
+                )}
+            </div>
             <div>
-                <button className="text-xl">
+                <button onClick={handleEdit} className="text-xl">
                     <MdEdit />
                 </button>
                 <button onClick={handleDelete} className="text-xl mx-4">
