e1c9c59b22c34d7740903cd8b5e9fbc7b2fdd2ca
aman-maharshi <amanm4harshi@gmail.com>
2022-10-21 21:45:54 +0530

    infinite-scrolling: design update

--- infinite-scrolling/src/App.js
+++ infinite-scrolling/src/App.js
@@ -16,7 +16,7 @@ const App = () => {
     const observer = useRef()
     const lastBookElementRef = useCallback(
         (node) => {
-            console.log("Last Book: ", node)
+            // console.log("Last Book: ", node)
 
             // if loading then we dont want to trigger infinite scrolling
             if (loading) return
@@ -27,7 +27,7 @@ const App = () => {
 
             observer.current = new IntersectionObserver((entries) => {
                 if (entries[0].isIntersecting) {
-                    console.log("visible")
+                    // console.log("visible")
                     setPageNumber((prevPageNumber) => prevPageNumber + 1)
                 }
             })
@@ -40,33 +40,64 @@ const App = () => {
     )
 
     return (
-        <section>
-            <input
-                type="text"
-                value={query}
-                onChange={handleSearchInputChange}
-                placeholder="Search"
-            />
-            <div>
+        <section className="md:w-8/12 mx-auto p-4">
+            <h1 className="text-center font-bold text-4xl my-4 tracking-wide">
+                BOOKS
+            </h1>
+            <p className="text-center mb-4">
+                API might take up to a minute to load the data.
+            </p>
+            <div className="bg-white shadow-xl p-2 text-xl w-full rounded-lg flex items-center gap-1">
+                <img src="/search.svg" alt="search" />
+                <input
+                    type="text"
+                    value={query}
+                    onChange={handleSearchInputChange}
+                    placeholder="Search"
+                    className="flex-1 bg-white p-2 focus:outline-none"
+                />
+            </div>
+            <div className="mt-6">
                 {books.map((item, index) => {
                     if (books.length === index + 1) {
                         return (
-                            <h3 ref={lastBookElementRef} key={index}>
-                                {item.title} ({item.first_publish_year})
-                            </h3>
+                            <div
+                                ref={lastBookElementRef}
+                                key={index}
+                                className="font-bold p-4 mb-2 flex justify-between bg-white border-b-2 border-[#f17a37] rounded-lg"
+                            >
+                                <div className="truncate">{item.title}</div>
+                                <div>{item.first_publish_year}</div>
+                            </div>
                         )
                     } else {
                         return (
-                            <h3 key={index}>
-                                {item.title} ({item.first_publish_year})
-                            </h3>
+                            <div
+                                key={index}
+                                className="font-bold p-4 mb-2 flex justify-between bg-white border-b-2 border-[#f17a37] rounded-lg"
+                            >
+                                <div className="truncate">{item.title}</div>
+                                <div>{item.first_publish_year}</div>
+                            </div>
                         )
                     }
                 })}
 
-                {loading && <h3>Loading...</h3>}
-                {error && <h3>Error...</h3>}
-                {!loading && books.length === 0 && <h3>No Data</h3>}
+                {loading && (
+                    <div className="animate-pulse w-full p-4 bg-white rounded-lg flex items-center justify-between">
+                        <p className="md:w-40 w-20 h-2 rounded-full bg-gray-300"></p>
+                        <p className="font-bold text-gray-400 text-lg">
+                            Loading...
+                        </p>
+                        <p className="md:w-40 w-20 h-2 rounded-full bg-gray-300"></p>
+                    </div>
+                )}
+                {error && <div className="font-bold">Error...</div>}
+                {!loading && books.length === 0 && (
+                    <div className="font-bold">
+                        No Data, search for a book to get results.
+                    </div>
+                )}
             </div>
         </section>
     )
--- infinite-scrolling/src/useBookSearch.js
+++ infinite-scrolling/src/useBookSearch.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react"
-import axios, { Axios } from "axios"
+import axios from "axios"
 
 const useBookSearch = (query, pageNumber) => {
     const [loading, setLoading] = useState(true)
