1c388f26d09d90a5350518d4532058820d8ea374
aman-maharshi <amanm4harshi@gmail.com>
2022-10-21 15:26:45 +0530

    infinite-scrolling: api call and showing results

--- infinite-scrolling/src/App.js
+++ infinite-scrolling/src/App.js
@@ -1,24 +1,37 @@
 import { useState, useEffect } from "react"
+import useBookSearch from "./useBookSearch"
 
 const App = () => {
-    const [query, setQuery] = useState("")
+    const [query, setQuery] = useState("woods")
     const [pageNumber, setPageNumber] = useState(1)
+
+    const { loading, error, books, hasMore } = useBookSearch(query, pageNumber)
+
+    const handleSearchInputChange = (e) => {
+        setQuery(e.target.value)
+        setPageNumber(1)
+    }
 
     return (
         <section>
-            <input type="text" placeholder="Search" />
+            <input
+                type="text"
+                value={query}
+                onChange={handleSearchInputChange}
+                placeholder="Search"
+            />
             <div>
-                <h3>Lorem, ipsum dolor.</h3>
-                <h3>Lorem, ipsum dolor.</h3>
-                <h3>Lorem, ipsum dolor.</h3>
-                <h3>Lorem, ipsum dolor.</h3>
-                <h3>Lorem, ipsum dolor.</h3>
-                <h3>Lorem, ipsum dolor.</h3>
-                <h3>Lorem, ipsum dolor.</h3>
-                <h3>Lorem, ipsum dolor.</h3>
+                {books.map((item) => {
+                    return (
+                        <h3 key={item}>
+                            {item.title} ({item.first_publish_year})
+                        </h3>
+                    )
+                })}
 
-                <h3>Loading...</h3>
-                <h3>Error</h3>
+                {loading && <h3>Loading...</h3>}
+                {error && <h3>Error...</h3>}
+                {!loading && books.length === 0 && <h3>No Data</h3>}
             </div>
         </section>
     )
--- infinite-scrolling/src/useBookSearch.js
+++ infinite-scrolling/src/useBookSearch.js
@@ -1,19 +1,55 @@
 import { useState, useEffect } from "react"
+import axios, { Axios } from "axios"
 
 const useBookSearch = (query, pageNumber) => {
+    const [loading, setLoading] = useState(true)
+    const [error, setError] = useState(false)
+    const [books, setBooks] = useState([])
+    const [hasMore, setHasMore] = useState(false)
+
     useEffect(() => {
-        getData()
+        setBooks([])
+    }, [query])
+
+    useEffect(() => {
+        setLoading(true)
+        let unmounted = false
+        let source = axios.CancelToken.source()
+        axios({
+            method: "GET",
+            url: "http://openlibrary.org/search.json",
+            params: { q: query, page: pageNumber },
+            cancelToken: source.token,
+        })
+            .then((res) => {
+                if (!unmounted) {
+                    console.log(res.data)
+                    setBooks((prevBooks) => {
+                        return [...new Set([...prevBooks, ...res.data.docs])]
+                    })
+                    setHasMore(res.data.docs.length > 0)
+                    setLoading(false)
+                }
+            })
+            .catch((e) => {
+                if (!unmounted) {
+                    setError(true)
+                    setLoading(false)
+                    if (axios.isCancel(e)) {
+                        console.log(`request cancelled:${e.message}`)
+                    } else {
+                        console.log("another error happened:" + e.message)
+                    }
+                }
+            })
+
+        return function () {
+            unmounted = true
+            source.cancel("Cancelling in cleanup")
+        }
     }, [query, pageNumber])
 
-    const getData = async () => {
-        const response = await fetch(
-            `http://openlibrary.org/search.json?q=one&page=1`
-        )
-        const content = await response.json()
-        console.log(content)
-    }
-
-    return null
+    return { loading, error, books, hasMore }
 }
 
 export default useBookSearch
