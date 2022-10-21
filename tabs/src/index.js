1afcf4ec7ec29d71158b4e5213e243c53febd8a6
aman-maharshi <amanm4harshi@gmail.com>
2022-10-21 22:28:41 +0530

    tabs: sass removed

--- tabs/src/index.js
+++ tabs/src/index.js
@@ -2,6 +2,6 @@ import React from "react"
 import ReactDOM from "react-dom"
 import App from "./App"
 
-import "./style.scss"
+import "./style.css"
 
 ReactDOM.render(<App />, document.getElementById("root"))
