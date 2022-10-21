328d4e61b7a5206cb09545e52c0e9b377959074e
aman-maharshi <amanm4harshi@gmail.com>
2022-10-21 22:40:14 +0530

    tabs: animating transitions using framer motion

--- tabs/src/CollectionItem.js
+++ tabs/src/CollectionItem.js
@@ -1,17 +1,25 @@
 import React from "react"
+import { motion } from "framer-motion"
 
 function CollectionItem({ items }) {
     return (
         <>
-            {items.map(item => {
+            {items.map((item) => {
                 return (
-                    <div key={item.id} className="item-wrapper">
+                    <motion.div
+                        layout
+                        animate={{ opacity: 1 }}
+                        initial={{ opacity: 0 }}
+                        exit={{ opacity: 0 }}
+                        key={item.id}
+                        className="item-wrapper"
+                    >
                         <img src={item.img} alt={item.title} />
                         <div className="item-details">
                             <span>{item.title}</span>
                             <span>&#8377; {item.price}</span>
                         </div>
-                    </div>
+                    </motion.div>
                 )
             })}
         </>
