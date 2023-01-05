04439e39bbbf6ffad71962ea5228414a4b26654c
aman-maharshi <amanm4harshi@gmail.com>
2023-01-05 19:03:33 +0530

    build v3

--- spell-checker/vite.config.js
+++ spell-checker/vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
-    base: "spell-checker"
+    base: "/spell-checker/",
+    plugins: [react()]
 })
