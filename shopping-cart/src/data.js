235c2967ae83089fd5b9e3a4fa9f567365691ab0
aman-maharshi <amanm4harshi@gmail.com>
2023-05-14 01:18:10 +0530

    image loading issue fix

--- shopping-cart/src/data.js
+++ shopping-cart/src/data.js
@@ -3,25 +3,25 @@ const data = [
         id: 1,
         name: "Shirt",
         price: 2500,
-        image: "/images/shirt.jpg"
+        image: "./images/shirt.jpg"
     },
     {
         id: 2,
         name: "Polo T Shirt",
         price: 1800,
-        image: "/images/polo.jpg"
+        image: "./images/polo.jpg"
     },
     {
         id: 3,
         name: "Sweater",
         price: 3200,
-        image: "/images/sweater.jpg"
+        image: "./images/sweater.jpg"
     },
     {
         id: 4,
         name: "T Shirt",
         price: 1500,
-        image: "/images/tshirt.jpg"
+        image: "./images/tshirt.jpg"
     }
 ]
 
