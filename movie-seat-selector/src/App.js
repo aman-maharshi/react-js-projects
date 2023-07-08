e7c196edd51e1bf40bd129ea760d908952a78f8b
aman-maharshi <amanm4harshi@gmail.com>
2023-07-08 21:25:25 +0530

    basic logic and UI implemented

---
+++ movie-seat-selector/src/App.js
@@ -0,0 +1,66 @@
+import { useState, useEffect } from 'react'
+import data from "./data.js"
+
+function App() {
+  const [seatsData, setSeatsData] = useState(data)
+  const [numberOfSeats, setNumberOfSeats] = useState(1)
+
+  const totalSelectedSeats = seatsData.reduce((accu, current) => current.status === 'occupied' ? accu + 1 : accu ,0)
+
+  const handleSeatClick = (key) => {
+    setSeatsData(prev => {
+      return prev.map(item => {
+        if (item.id >= key && item.id < key + numberOfSeats) {
+          return {
+            ...item,
+            status: 'occupied'
+          }
+        } else {
+          return item
+        }
+      })
+    })
+  }
+
+  const clearSelectedSeats = () => {
+    setSeatsData(prev => prev.map(item => ({...item, status: 'vacant'})))
+  }
+
+  return (
+    <div className='wrapper'>
+      <div className='selector'>
+        <h1 className='selector__title'>How Many Seats?</h1>
+        <div className='selector__input-row'>
+          <input 
+            type="range" 
+            value={numberOfSeats} 
+            onChange={e => setNumberOfSeats(Number(e.target.value))} 
+            min={1} 
+            max={5} 
+          />
+          <div className='label'>{numberOfSeats}</div>
+        </div>
+      </div>
+      <div className='seats'>
+        {seatsData.map(item => {
+          const {id, status} = item
+          return (
+            <div 
+              key={id} 
+              className={status === "occupied" ? "seat occupied" : "seat"}
+              onClick={() => handleSeatClick(id)}
+            >
+              {id}
+            </div>
+          )
+        })}
+      </div>
+      <div className="info-row">
+        <div>Selected Seats: {totalSelectedSeats}</div>
+        <button onClick={clearSelectedSeats}>Clear All</button>
+      </div>
+    </div>
+  )
+}
+
+export default App;
---
+++ movie-seat-selector/src/data.js
@@ -0,0 +1,64 @@
+const data = [
+  {
+    id: 1,
+    status: 'vacant'
+  },
+  {
+    id: 2,
+    status: 'vacant'
+  },
+  {
+    id: 3,
+    status: 'vacant'
+  },
+  {
+    id: 4,
+    status: 'vacant'
+  },
+  {
+    id: 5,
+    status: 'vacant'
+  },
+  {
+    id: 6,
+    status: 'vacant'
+  },
+  {
+    id: 7,
+    status: 'vacant'
+  },
+  {
+    id: 8,
+    status: 'vacant'
+  },
+  {
+    id: 9,
+    status: 'vacant'
+  },
+  {
+    id: 10,
+    status: 'vacant'
+  },
+  {
+    id: 11,
+    status: 'vacant'
+  },
+  {
+    id: 12,
+    status: 'vacant'
+  },
+  {
+    id: 13,
+    status: 'vacant'
+  },
+  {
+    id: 14,
+    status: 'vacant'
+  },
+  {
+    id: 15,
+    status: 'vacant'
+  },
+]
+
+export default data
